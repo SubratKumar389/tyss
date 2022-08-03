6a93679f85852ee7087444075096a804821a0256
testgithubbri <subratkumarsahu402@gmail.com>
2022-08-03 11:11:24 +0530

    commite

---
+++ MAVENEXAMPLE/src/test/java/org/grid/practice/GridPracticeTest.java
@@ -0,0 +1,24 @@
+package org.grid.practice;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class GridPracticeTest {
+
+	public static void main(String[] args) throws MalformedURLException {
+		
+ URL url = new URL("http://localhost:4444/wd/hub");
+ DesiredCapabilities cap=new DesiredCapabilities();
+ 
+ cap.setBrowserName("chrome");
+ cap.setPlatform(Platform.WINDOWS);
+ RemoteWebDriver driver=new RemoteWebDriver(url, cap);
+ 
+ driver.get("https://youtube.com");
+	}
+
+}
