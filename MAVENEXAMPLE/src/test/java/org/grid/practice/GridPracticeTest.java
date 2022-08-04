9adb8a2d035bf92ed43222cb33e125765aefffac
testgithubbri <subratkumarsahu402@gmail.com>
2022-08-04 14:26:43 +0530

    vy

--- MAVENEXAMPLE/src/test/java/org/grid/practice/GridPracticeTest.java
+++ MAVENEXAMPLE/src/test/java/org/grid/practice/GridPracticeTest.java
@@ -11,9 +11,9 @@ public class GridPracticeTest {
 
 	public static void main(String[] args) throws MalformedURLException {
 		
-// URL url = new URL("http://localhost:4444/wd/hub");
-// DesiredCapabilities cap=new DesiredCapabilities();
- 
+ URL url = new URL("http://localhost:4444/wd/hub");
+ DesiredCapabilities cap=new DesiredCapabilities();
+ //heloo
  cap.setBrowserName("chrome12");
  cap.setPlatform(Platform.WINDOWS);
  RemoteWebDriver driver=new RemoteWebDriver(url, cap);
